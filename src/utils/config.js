e4f4ffa3876649fd637cb6d343260c2d79e6734a
kulendra <kulendra007@gmail.com>
2020-04-24 15:23:38 -0400

    changed description and added styling

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 export const params = {
     "WEB_PAGE_TITLE" : "Kulendra Kumar Kaushal",
     "MAIN_PAGE_TITLE": "Cloudflare Summer Internship Application",
-    "DESCRIPTION" : "Hello, I am Kulendra, doing MS in CS from Virginia Tech and have two years of relevant experience in the field of software development. I was supposed to do an internship at Berkeley Net, Summer 2020. Unfortunately because of COVID19, my internship offer got rescinded. Please visit my LinkedIn profile to find more details about me and my projects. I am really thankful to Cloudflare team for providing me an opportunity to learn about cloudflare workers. I am amused that how easily high performance serverless web application can be build and deployed within few seconds using Cloudflare Edge Network. It was really a great learning experience for me. Stay safe and enjoy!",
+    "DESCRIPTION" : "Hello, I am Kulendra, doing MS in CS from Virginia Tech and have two years of relevant experience in the field of software development. I was supposed to do an internship in the Summer of 2020. Unfortunately because of COVID19, my internship offer got rescinded. Please visit my LinkedIn profile to find more details about me and my projects. I am really thankful to Cloudflare team for providing me an opportunity to learn about cloudflare workers. I am amazed that how easily high performance serverless web application can be build and deployed within few seconds using Cloudflare Edge Network. It was really a great learning experience for me. Stay safe and enjoy!",
     "REDIRECT_URL": "https://linkedin.com/in/kulendra-kumar-kaushal",
     "REDIRECT_URL_TEXT": "My LinkedIn",
     "CLOUDFLARE_API": "https://cfw-takehome.developers.workers.dev/api/variants",
--- src/utils/html_write_handler.js
+++ src/utils/html_write_handler.js
@@ -2,15 +2,19 @@ import {params} from "./config";
 
 class ElementHandler {
     constructor(variant_id) {
-        this.variant_id = variant_id;
+        this.variant_id = variant_id + 1;
     }
     element(element) {
         if (element.tagName === 'title')
             element.setInnerContent(params['WEB_PAGE_TITLE']);
         else if (element.tagName === 'h1' && element.getAttribute('id') === 'title')
             element.setInnerContent(params['MAIN_PAGE_TITLE'] + " \nVariant" + this.variant_id);
-        else if (element.tagName === 'p' && element.getAttribute('id') === 'description')
+        else if (element.tagName === 'p' && element.getAttribute('id') === 'description'){
             element.setInnerContent(params['DESCRIPTION']);
+            element.setAttribute("style", "text-align: justify !important")
+            element.setAttribute("style", "text-justify: inter-word !important")
+            element.setAttribute("style", "text-center: !important")
+        }
         else if (element.tagName === 'a' && element.getAttribute('id') === 'url') {
             element.setInnerContent(params['REDIRECT_URL_TEXT']);
             element.setAttribute('href', params['REDIRECT_URL']);
